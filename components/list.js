60980bf1d70fbd7458c119b4bd7e8897eece24aa
Abdullah Koroglu <abdullah34koroglu@gmail.com>
2021-02-09 02:07:21 +0300

    added page feature

--- components/list.js
+++ components/list.js
@@ -2,12 +2,13 @@ import Card from "../components/card";
 import { Heading, Link, Text, Code, Container, Box, Flex, Button, Skeleton } from "@chakra-ui/react";
 
 
-export default function List({data}) {
+export default function List({data , page}) {
+  page = !  isNaN(page) ?  page : 0
   return (
     <Flex flexDirection='column' justify='left' >
-        {data.length !== 0 ?
-          data.map(i => (
-            <Card HbName={i.title}  AmzName={i.title}  persantage={i.id}  HbPrice={i.id}  AmzPrice={i.id} />
+        {data !== [] ?
+          data.slice((page )* 20,(page + 1)* 20 ).map(i => (
+            <Card HbName={i.title}  AmzName={i.title}  persantage={i.id}  HbPrice={i.id}  AmzPrice={i.id} key={i.id}/>
           )) : <SkeletonBucket />
         }
       </Flex>
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,32 @@
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import List from "../components/list";
 import { Heading, Link, Text, Code, Container, Box, Flex, Button, Skeleton } from "@chakra-ui/react";
 
 
-export default function Home() {
-  const [data, setData] = useState([])
-  useEffect(() => {
-    async function func() {
-      var data1 = await fetch('https://jsonplaceholder.typicode.com/todos')
-      var parsed = await data1.json()
-      await setData(parsed)
-      console.log(data);
-    }
-    func()
-  }, []);
+export default function Home({data}) {
+  const router = useRouter()
+  // const [data, setData] = useState([])
+  // useEffect(() => {
+  //   async function func() {
+  //     var data1 = await fetch('https://jsonplaceholder.typicode.com/todos')
+  //     var parsed = await data1.json()
+  //     await setData(parsed)
+  //     console.log(data);
+  //   }
+  //   func()
+  // }, []);
   return (
     <div>
-      <List data={data}/>
+      <List data={data} page={router.query.page - 1}/>
     </div>
   )
 }
 
 
 
-// Home.getInitialProps = async()=>{
-// }
+Home.getInitialProps = async()=>{
+  var response = await fetch('https://jsonplaceholder.typicode.com/todos')
+  var data = await response.json()
+  return {data}
+}
